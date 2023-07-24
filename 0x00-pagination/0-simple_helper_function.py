7c0152726ee936763338b09dac31c6ee48d567fe
myrajarenga <myrajarenga1234@gmail.com>
2023-07-24 07:34:00 -0700

    simple helper fucntion

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
 function to return a tupleof size two
-a start index and an end index 
+a start index and an end index
 """
 
 
 def index_range(page, page_size) -> tuple:
-    # Calculate the start  and  last index for the given page and page_size
+    """Calculate the start  and  last index 
+    for the given page and page_size
+    """
     return (((page - 1) * page_size), page * page_size)
+
 
 if __name__ == "__main__":
     res = index_range(1, 7)
