8d4d01f4646c42e3b3b917915cc54458b3b288bc
myrajarenga <myrajarenga1234@gmail.com>
2023-07-26 07:27:56 -0700

    implimenting basic dictionary in acache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,16 +6,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-     """defines basic cashing"""
+    """defines basic cashing"""
     def __init__(self):
         super().__init__()
 
     def put(self, key, item):
         """assigns key and item to the dict"""
-        if key is not None and item is not None:
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
         """retrives the value of the key from the dict"""
-        if key is not None:
-            return self.cache_data.get(key)
+        if key is None or self.cache_data.get(key) is None:
+            return None
