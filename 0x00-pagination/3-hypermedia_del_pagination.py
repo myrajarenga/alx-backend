0a94764916f835873b2cd79e95faaba339d14e34
myrajarenga <myrajarenga1234@gmail.com>
2023-07-28 02:19:26 -0700

    handlingwq cases where some rows have been deleted and provide the correct page of the dataset based on the given index and page size.

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-         """ return all data"""
+        """ return all data"""
         try:
             assert index is None or \
                     (type(index) is int and index >= 0), "Invalid index"
