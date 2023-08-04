6107e1001de8a1cf95cf2faf77c13daed16ed030
myrajarenga <myrajarenga1234@gmail.com>
2023-08-04 08:50:38 -0700

    rendering 1-index.html template

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -33,7 +33,7 @@ babel = Babel(app)
 @app.route('/')
 def index():
     """"function  to render index file"""
-    return render_template('index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
